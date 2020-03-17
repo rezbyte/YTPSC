5bbeec19089f2f320f7fdfb50965107e45aec1ea
rezbyte <48037381+rezbyte@users.noreply.github.com>
2020-03-17 23:56:19 +0800

    Changed indenting on slider.js

--- slider.js
+++ slider.js
@@ -1,12 +1,13 @@
+console.log("slider.js called");
 var slider = document.getElementById("theSlider");
 
-  slider.oninput = function() {
-    changeSpeed(this.value);
-  }
+slider.oninput = function() {
+  changeSpeed(this.value);
+}
 
-  function changeSpeed(desiredSpeed) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(tabs[0].id,
-        {code: 'document.getElementsByTagName("video")[0].playbackRate = "' + desiredSpeed + '";'});
-    });
-  }
+function changeSpeed(desiredSpeed) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.executeScript(tabs[0].id,
+      {code: 'document.getElementsByTagName("video")[0].playbackRate = "' + desiredSpeed + '";'});
+  });
+}
