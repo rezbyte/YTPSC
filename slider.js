b15156fa1db90edc01c96d9d82471b8396f7678c
rezbyte <48037381+rezbyte@users.noreply.github.com>
2020-03-18 00:12:57 +0800

    Added a reset button

--- slider.js
+++ slider.js
@@ -1,12 +1,18 @@
-var slider = document.getElementById("theSlider");
+var slider = document.getElementById("slider_Speed");
+var button = document.getElementById("button_Reset");
 
 slider.oninput = function() {
   changeSpeed(this.value);
 }
 
+button.click = function() {
+  changeSpeed(0.1);
+  slider.value = 0.1;
+}
+
 function changeSpeed(desiredSpeed) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.executeScript(tabs[0].id,
-      {code: 'document.getElementsByTagName("video")[0].playbackRate = "' + desiredSpeed + '";'});
+    {code: 'document.getElementsByTagName("video")[0].playbackRate = "' + desiredSpeed + '";'});
   });
 }
