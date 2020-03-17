4f9d447cafb676afdb2192cca327cb680c4f59f2
rezbyte <48037381+rezbyte@users.noreply.github.com>
2020-03-17 22:12:43 +0800

    Added onInstalled() to handler.js

--- handler.js
+++ handler.js
@@ -1,16 +1,19 @@
-var slider = document.getElementById("theSlider");
-var currentLocation = window.location.href.toString();
+chrome.runtime.onInstalled.addListener(function() {
+  var slider = document.getElementById("theSlider");
+  var currentLocation = window.location.href.toString();
 
-if (currentLocation.includes("youtube.com")) {
-  slider.disabled = false;
-  slider.oninput = function() {
-    changeSpeed(this.value);
+  if (currentLocation.includes("youtube.com")) {
+    slider.disabled = false;
+    slider.oninput = function() {
+      changeSpeed(this.value);
+    }
+  } else {
+    slider.disabled = true;
   }
-} else {
-  slider.disabled = true;
-}
 
 
-function changeSpeed(desiredSpeed) {
-  document.getElementsByTagName("video")[0].playbackRate = desiredSpeed;
-}
+  function changeSpeed(desiredSpeed) {
+    document.getElementsByTagName("video")[0].playbackRate = desiredSpeed;
+  }
+
+});
